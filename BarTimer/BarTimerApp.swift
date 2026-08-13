eaf42a316eee219acd709a24aced8239b180bcb9
Daniel Moreno <123407190+danmoree@users.noreply.github.com>
2026-08-13 13:27:52 -0600

    Initial Commit

---
+++ BarTimer/BarTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarTimerApp.swift
+//  BarTimer
+//
+//  Created by Daniel Moreno on 8/13/26.
+//
+
+import SwiftUI
+
+@main
+struct BarTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
